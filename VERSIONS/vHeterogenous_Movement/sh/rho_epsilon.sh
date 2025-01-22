cbfec4c3a9fb2fe63cc6a9fb1502adc3748335fa
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2025-01-22 09:53:18 +0100

    adapting path for results

--- VERSIONS/vHeterogenous_Movement/sh/rho_epsilon.sh
+++ VERSIONS/vHeterogenous_Movement/sh/rho_epsilon.sh
@@ -13,6 +13,8 @@ for rho in $(seq 0.001 0.1 1.001)
 do
     for epsilon in $(seq 0.001 0.1 1.001)
     do
-        python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho_epsilon/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} --social_feedbacks both -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
+        # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho_epsilon/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} --social_feedbacks both -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
+        python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2025/rho_epsilon_with_tags/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} --social_feedbacks both -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
+
     done
 done
