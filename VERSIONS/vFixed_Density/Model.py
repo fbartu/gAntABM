bc7df84fc845274e543b3a3bd69ea07751e0e44c
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2025-02-13 13:11:21 +0100

    debugging continues

--- VERSIONS/vFixed_Density/Model.py
+++ VERSIONS/vFixed_Density/Model.py
@@ -97,7 +97,7 @@ class Model(Model):
         self.food_condition = food_condition
         self.init_food()
         self.food_coords = [self.coords[i] for i in self.food_positions]
-        print(self.food_coords)
+        print(self.food_coords, flush = True)
 
 # 		self.data = pd.DataFrame({'T': [], 'Frame': [],
 #    'N': [], 'Si_out': [], 'pos': [],
